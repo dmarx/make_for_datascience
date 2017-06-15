f8d1de0b8e7f1a88cf173fbb043c2525368ccc1b
David Marx <davidmarx@cho.elderresearch.com>
2017-06-15 16:30:47 +0100

    Updated eval_model to use

--- src/eval/eval_model.r
+++ src/eval/eval_model.r
@@ -7,8 +7,7 @@ scored = predict(mod, newdata=test, type='response')
 confusion <- table(test$Species, scored>.5)
 
 # Assume mod_path is of the form `models/mod_name.rdata`
-path_parts = strsplit(mod_path, "/")[[1]]
-fname = path_parts[[length(path_parts)]]
+fname = basename(mod_path)
 mod_name = strsplit(fname, '.rdata')[1]
 
 fname = paste0("./reports/confusion_matrix_", mod_name, ".txt")

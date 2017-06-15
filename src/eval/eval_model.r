21e2827630b183d5aa28fb8f82584339620ec0a6
David Marx <davidmarx@cho.elderresearch.com>
2017-06-15 16:45:35 +0100

    keep file extension in model object name in reports

--- src/eval/eval_model.r
+++ src/eval/eval_model.r
@@ -7,8 +7,7 @@ scored = predict(mod, newdata=test, type='response')
 confusion <- table(test$Species, scored>.5)
 
 # Assume mod_path is of the form `models/mod_name.rdata`
-fname = basename(mod_path)
-mod_name = strsplit(fname, '.rdata')[1]
+mod_name = basename(mod_path)
 
 fname = paste0("./reports/confusion_matrix_", mod_name, ".txt")
 write.csv(confusion, file = fname)
